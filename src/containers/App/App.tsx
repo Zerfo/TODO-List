886e4da16af2c62dbdf8f1e5e087c53972e13758
Nikolay Smekalin <nikolay.smekalin@gmail.com>
2024-03-11 12:47:26 +0300

    feat: add empty data window

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -5,12 +5,17 @@ import Divider from 'components/Divider';
 import Title from 'components/Title';
 
 import { useAppDispatch } from 'store';
+import { useTasks } from 'store/tasks/hooks';
 import { fetchTasks } from 'store/tasks/thunks';
+
+import emptyImage from 'assets/images/empty-data.png';
 
 import styles from './App.module.scss';
 
 function App() {
   const dispatch = useAppDispatch();
+
+  const tasks = useTasks();
 
   useEffect(() => {
     dispatch(fetchTasks());
@@ -21,6 +26,13 @@ function App() {
       <Block className={styles.wrapper}>
         <Title level={3}>My Tasks</Title>
         <Divider />
+
+        {(!tasks || tasks.length === 0) && (
+        <div className={styles.emptyWrapper}>
+          <img src={emptyImage} alt="empty" />
+          <p>No data</p>
+        </div>
+        )}
       </Block>
     </div>
   );
