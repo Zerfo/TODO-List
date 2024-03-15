1a254be2922e3ff71b5c3000ae478a57dfbe8ed2
Nikolay Smekalin <nikolay.smekalin@gmail.com>
2024-03-15 13:07:45 +0300

    chore: add svgr plugin

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginCssMinimizer } from '@rsbuild/plugin-css-minimizer';
 import { pluginReact } from '@rsbuild/plugin-react';
+import { pluginSvgr } from '@rsbuild/plugin-svgr';
 import { pluginTypeCheck } from '@rsbuild/plugin-type-check';
 
 export default defineConfig({
-  plugins: [pluginReact(), pluginTypeCheck(), pluginCssMinimizer()],
+  plugins: [
+    pluginReact(),
+    pluginSvgr({
+      svgrOptions: {
+        exportType: 'default',
+      },
+    }),
+    pluginTypeCheck(),
+    pluginCssMinimizer(),
+  ],
   html: {
     title: 'React To-Do List',
     template: './src/index.html',
